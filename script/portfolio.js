265b2d18c93994ce7a97219a2c18bdc60d6a9e5b
kim <gangssu0@gmail.com>
2023-09-04 01:56:23 +0900

    first

---
+++ script/portfolio.js
@@ -0,0 +1,28 @@
+const closeBtn = document.querySelector('.btn .close_btn')
+const logo = document.querySelector('.logo')
+const openLogo = document.querySelector('.open_logo')
+const nav = document.querySelector('nav > ul')
+const mNav = document.querySelector('.m_nav')
+const openBtn = document.querySelector('.open_btn')
+console.log(mNav, logo, openLogo, nav, openBtn)
+mNav.style.display = 'none'
+nav.style.filter = 'opacity(0)'
+nav.style.transform = 'translateY(30px)'
+openLogo.style.filter = 'opacity(0)'
+closeBtn.style.display = 'none'
+openBtn.addEventListener('click',function(){
+    openBtn.style.display ='none'
+    closeBtn.style.display = 'flex'
+    nav.style.filter = 'opacity(100%)'
+    nav.style.transform = 'translateY(0)'
+    openLogo.style.filter = 'opacity(100%)'
+    logo.style.filter = 'opacity(0)'
+})
+closeBtn.addEventListener('click',function(){
+    closeBtn.style.display = 'none'
+    openBtn.style.display = 'flex'
+    nav.style.filter = 'opacity(0)'
+    nav.style.transform = 'translateY(30px)'
+    openLogo.style.filter = 'opacity(0)'
+    logo.style.filter = 'opacity(100%)'
+})
